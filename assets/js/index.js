67d866a3d02346b39d5312cccf09f5560213fe9a
Ramiro Sepulveda <rgsepulveda@uc.cl>
2024-03-09 17:37:32 -0300

    Desafio completo

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+function primero() {
+    var primero = document.querySelector("#primero");
+    var segundo = document.querySelector("#segundo");
+    var tercero = document.querySelector("#tercero");
+    primero.style.display = "block";
+    segundo.style.display = "none";
+    tercero.style.display = "none";
+}
+
+function segundo() {
+    var primero = document.querySelector("#primero");
+    var segundo = document.querySelector("#segundo");
+    var tercero = document.querySelector("#tercero");
+    primero.style.display = "none";
+    segundo.style.display = "block";
+    tercero.style.display = "none";
+}
+
+function tercero() {
+    var primero = document.querySelector("#primero");
+    var segundo = document.querySelector("#segundo");
+    var tercero = document.querySelector("#tercero");
+    primero.style.display = "none";
+    segundo.style.display = "none";
+    tercero.style.display = "block";
+} 
