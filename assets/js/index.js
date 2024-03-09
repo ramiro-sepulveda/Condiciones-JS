0fd0c2dbdfd38a6a0df93cdabb2026218633871d
Ramiro Sepulveda <rgsepulveda@uc.cl>
2024-03-09 18:19:15 -0300

    Agrega efecto boton activo

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,25 +2,43 @@ function primero() {
     var primero = document.querySelector("#primero");
     var segundo = document.querySelector("#segundo");
     var tercero = document.querySelector("#tercero");
+    var btn1 = document.querySelector("#btn1");
+    var btn2 = document.querySelector("#btn2");
+    var btn3 = document.querySelector("#btn3");
     primero.style.display = "block";
     segundo.style.display = "none";
     tercero.style.display = "none";
+    btn1.style.backgroundColor = "#105132";
+    btn2.style.backgroundColor = "#198754";
+    btn3.style.backgroundColor = "#198754";
 }
 
 function segundo() {
     var primero = document.querySelector("#primero");
     var segundo = document.querySelector("#segundo");
     var tercero = document.querySelector("#tercero");
+    var btn1 = document.querySelector("#btn1");
+    var btn2 = document.querySelector("#btn2");
+    var btn3 = document.querySelector("#btn3");
     primero.style.display = "none";
     segundo.style.display = "block";
     tercero.style.display = "none";
+    btn1.style.backgroundColor = "#198754";
+    btn2.style.backgroundColor = "#105132";
+    btn3.style.backgroundColor = "#198754";
 }
 
 function tercero() {
     var primero = document.querySelector("#primero");
     var segundo = document.querySelector("#segundo");
     var tercero = document.querySelector("#tercero");
+    var btn1 = document.querySelector("#btn1");
+    var btn2 = document.querySelector("#btn2");
+    var btn3 = document.querySelector("#btn3");
     primero.style.display = "none";
     segundo.style.display = "none";
     tercero.style.display = "block";
+    btn1.style.backgroundColor = "#198754";
+    btn2.style.backgroundColor = "#198754";
+    btn3.style.backgroundColor = "#105132";
 } 
